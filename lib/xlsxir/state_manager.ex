90b256587bcaf2546abdbd5042d42403524f5df3
wyp169900 <39908860+wyp169900@users.noreply.github.com>
2018-10-29 20:25:42 +0800

    Update state_manager.ex

--- lib/xlsxir/state_manager.ex
+++ lib/xlsxir/state_manager.ex
@@ -8,6 +8,10 @@ defmodule Xlsxir.StateManager do
   def start_link() do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
+  
+  def init(args) do
+    {:ok, args}
+  end
 
   def handle_call(:new_table, _from, state) do
     {:reply, :ets.new(:table, [:set, :public]), state}
